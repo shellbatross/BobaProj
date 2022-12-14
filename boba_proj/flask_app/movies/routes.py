130cbba0cbed0df53af89eb078b96c78be0e3b42
Shell <stephanie16066@gmail.com>
2022-12-14 10:33:58 -0500

    aaaaa

---
+++ boba_proj/flask_app/movies/routes.py
@@ -0,0 +1,69 @@
+from flask import Blueprint, render_template, url_for, redirect, request, flash
+from flask_login import current_user
+
+from .. import movie_client
+from ..forms import MovieReviewForm, SearchForm
+from ..models import User, Review
+from ..utils import current_time
+
+movies = Blueprint("movies", __name__)
+
+
+""" ************ View functions ************ """
+
+
+@movies.route("/", methods=["GET", "POST"])
+def index():
+    form = SearchForm()
+
+    if form.validate_on_submit():
+        return redirect(url_for("movies.query_results", query=form.search_query.data))
+
+    return render_template("index.html", form=form)
+
+
+@movies.route("/search-results/<query>", methods=["GET"])
+def query_results(query):
+    try:
+        results = movie_client.search(query)
+    except ValueError as e:
+        flash(str(e))
+        return redirect(url_for("movies.index"))
+
+    return render_template("query.html", results=results)
+
+
+@movies.route("/movies/<movie_id>", methods=["GET", "POST"])
+def movie_detail(movie_id):
+    try:
+        result = movie_client.retrieve_movie_by_id(movie_id)
+    except ValueError as e:
+        flash(str(e))
+        return redirect(url_for("users.login"))
+
+    form = MovieReviewForm()
+    if form.validate_on_submit() and current_user.is_authenticated:
+        review = Review(
+            commenter=current_user._get_current_object(),
+            content=form.text.data,
+            date=current_time(),
+            imdb_id=movie_id,
+            movie_title=result.title,
+        )
+        review.save()
+
+        return redirect(request.path)
+
+    reviews = Review.objects(imdb_id=movie_id)
+
+    return render_template(
+        "movie_detail.html", form=form, movie=result, reviews=reviews
+    )
+
+
+@movies.route("/user/<username>")
+def user_detail(username):
+    user = User.objects(username=username).first()
+    reviews = Review.objects(commenter=user)
+
+    return render_template("user_detail.html", username=username, reviews=reviews)
---
+++ boba_proj/flask_app/users/routes.py
@@ -0,0 +1,77 @@
+from flask import Blueprint, redirect, url_for, render_template, flash, request
+from flask_login import current_user, login_required, login_user, logout_user
+
+from .. import bcrypt
+from ..forms import RegistrationForm, LoginForm #,UpdateUsernameForm
+from ..models import User
+
+"""
+Definitely keeping these to use as reference for when we make our own blueprints
+"""
+
+users = Blueprint("users", __name__)
+
+""" ************ User Management views ************ """
+
+
+@users.route("/register", methods=["GET", "POST"])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for("movies.index"))
+
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed = bcrypt.generate_password_hash(form.password.data).decode("utf-8")
+        user = User(username=form.username.data, email=form.email.data, password=hashed)
+        user.save()
+
+        return redirect(url_for("users.login"))
+
+    return render_template("register.html", title="Register", form=form)
+
+
+@users.route("/login", methods=["GET", "POST"])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for("movies.index"))
+
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.objects(username=form.username.data).first()
+
+        if user is not None and bcrypt.check_password_hash(
+            user.password, form.password.data
+        ):
+            login_user(user)
+            return redirect(url_for("users.account"))
+        else:
+            flash("Login failed. Check your username and/or password")
+            return redirect(url_for("users.login"))
+
+    return render_template("login.html", title="Login", form=form)
+
+
+@users.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("movies.index"))
+
+""""
+@users.route("/account", methods=["GET", "POST"])
+@login_required
+def account():
+    username_form = UpdateUsernameForm()
+
+    if username_form.validate_on_submit():
+        # current_user.username = username_form.username.data
+        current_user.modify(username=username_form.username.data)
+        current_user.save()
+        return redirect(url_for("users.account"))
+
+    return render_template(
+        "account.html",
+        title="Account",
+        username_form=username_form,
+    )
+"""
